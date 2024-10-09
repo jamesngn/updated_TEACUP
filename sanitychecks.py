42383203c75c1dc1c6bd2d2f0a5645a397b2876e
Quang Nguyen <jamesnguyennm@gmail.com>
2024-10-10 03:30:53 +1100

    Refactor sanitychecks.py to use fabric2's SerialGroup for running sanity checks on multiple hosts and update config variable name

--- sanitychecks.py
+++ sanitychecks.py
@@ -728,12 +728,17 @@ def sanity_checks_v2(c):
     group = SerialGroup(*all_hosts)
     
     print("group: ", group)
-    # configV2 = Config.from_v1(env)
+    env.ssh_config_path = ""
     
-    # print("config: ", configV2)
+    configV2 = Config(overrides={
+    "connect_kwargs": {
+        "password": "password"
+    }
+})
+    
+    print("config: ", configV2)
 
 
-    print("env: ", env)    
     # for conn in group:
     #     print(f"Running sanity checks on {conn.host}")
     #     check_host_v2(conn)
