71c021011b2e1cbd7866028e8b192be7ad9d0ae9
Quang Nguyen <jamesnguyennm@gmail.com>
2024-10-10 21:01:58 +1100

    Refactor get_netmac_v2 function in hostmac.py to use get_type_cached_v2 instead of get_type_cached and remove debug print statement

--- hostmac.py
+++ hostmac.py
@@ -215,12 +215,11 @@ def get_netmac_v2(c: Connection, internal_int='0') -> str:
                 "awk '{ printf(\"%s %s\\n\", $2, $4) }'",
                 hide=True, echo=True, echo_format=f"[{c.host}]: {{command}}").stdout
         elif htype == 'Linux':
-            print(f"[{c.host}]: Getting MAC address for {host_string}")
             macips = c.run(
                 "ifconfig | awk '/HWaddr / { printf(\"%s \", $0); next } 1' | "
                 "grep HWaddr | grep 'inet ' | "
                 "awk '{ printf(\"%s %s\\n\", $5, $7) }' | sed -e 's/addr://'",
-                hide=True, echo=True, echo_format=f"[{c.host}]: {{command}}").stdout
+                echo=True, echo_format=f"[{c.host}]: {{command}}").stdout.strip()
         else:
             raise RuntimeError(f"Can't determine MAC address for OS {htype}")
 
