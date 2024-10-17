9c71573a9dfa1ce1aeaf0b9bcfe9e04ed63eff4b
Quang Nguyen <jamesnguyennm@gmail.com>
2024-10-17 22:02:15 +1100

    Refactor config.py to update host IP addresses and connections

--- config.py
+++ config.py
@@ -37,12 +37,12 @@ TPCONF_debug_level = 0
 
 # Host lists
 TPCONF_router = ['router', ]
-TPCONF_hosts = [ 'host4', ]
+TPCONF_hosts = [ 'host1', 'host4', ]
 
 # Map external IPs to internal IPs
 TPCONF_host_internal_ip = {
     'router': ['172.16.10.1', '172.16.11.1'],
-    # 'host2':  ['172.16.10.3'],
+    'host1':  ['172.16.10.2'],
     'host4':  ['172.16.11.2'],
 }
 
@@ -95,7 +95,7 @@ TPCONF_os_partition = {
 
 TPCONF_host_os = {
     'router': 'Linux',
-    # 'host2': 'Linux',
+    'host1': 'Linux',
     'host4': 'Linux',
 }
 
@@ -151,14 +151,14 @@ TPCONF_router_queues = [
 traffic_iperf = [
     # Specifying external addresses traffic will be created using the _first_
     # internal addresses (according to TPCONF_host_internal_ip)
-    ('0.0', '1', " start_iperf, client='router', server='host4', port=5000, "
+    ('0.0', '1', " start_iperf, client='host1', server='host4', port=5000, "
      " duration=V_duration "),
-    # ('0.0', '2', " start_iperf, client='host2', server='host4', port=5001, "
-    #  " duration=V_duration "),
-    # ('0.0', '3', " start_iperf, client='host4', server='host2', port=5000, "
-    #  " duration=V_duration "),
-    # ('0.0', '4', " start_iperf, client='host4', server='host2', port=5001, "
-    #  " duration=V_duration "),
+    ('0.0', '2', " start_iperf, client='host1', server='host4', port=5001, "
+     " duration=V_duration "),
+    ('0.0', '3', " start_iperf, client='host4', server='host1', port=5000, "
+     " duration=V_duration "),
+    ('0.0', '4', " start_iperf, client='host4', server='host1', port=5001, "
+     " duration=V_duration "),
     # Or using internal addresses
     #( '0.0', '1', " start_iperf, client='192.168.10.11', server='192.168.10.12', "
     #              " port=5000, duration=V_duration " ),
@@ -297,7 +297,7 @@ hostConfig = Config(overrides={
 
 host_to_conn = {
     'controlhost': Connection('controlhost', config=hostConfig),
-    # 'host2': Connection('host2', config=hostConfig),
+    'host1': Connection('host1', config=hostConfig),
     'host4': Connection('host4', config=hostConfig),
 }
 
